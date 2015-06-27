22f98e2a851d49de38b0a3746833bfac85a5e070
Matt Krick <matt.krick@gmail.com>
2015-06-27 10:43:59 -0500

    update git repo in package

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A tooltips wrapper with material design animation',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/mattkrick/meteorTooltips',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
